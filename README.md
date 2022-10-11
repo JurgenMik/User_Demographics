7f33ba86381364be2bedab20f1560812fdccb293
Jürgen Mikk <jurgen.mikk@khk.ee>
2022-10-11 20:32:52 +0300

    Update ReadMe

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# Getting Started with Create React App
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+User Demographics
+## Table of Contents
+* <strong>Overview</strong>
+    * <strong>Functionality</strong>
+    * <strong>Screenshots</strong>
+    * <strong>Built with</strong>
+### App. Functionalities
+* <strong>Detailed</strong> view of personal information
+* <strong>Create</strong> new users
+* <strong>Edit</strong> user information
+* <strong>Delete</strong> users
+* <strong>Search</strong> for users by first or last name
+### Screenshots
+![image](https://user-images.githubusercontent.com/89903354/195159376-5f1041fe-8403-4f05-9998-00cf4a19b794.png)
+![image](https://user-images.githubusercontent.com/89903354/195159693-f42aaa94-d652-4f0a-9483-15ed82772b23.png)
+![image](https://user-images.githubusercontent.com/89903354/195159859-26e26de9-c797-41cd-bd8c-fdd8117bb303.png)
+![image](https://user-images.githubusercontent.com/89903354/195160061-d1a05e05-39c2-482a-a6cd-82ef8e8f9095.png)
+### Built With
+* <strong>React</strong>
+* <strong>TypeScript</strong>
+* <strong>TailwindCSS - CSS framework</strong>
--- src/components/CreateUserModal.tsx
+++ src/components/CreateUserModal.tsx
@@ -43,45 +43,45 @@ function CreateUserModal({handleCancel} : any) {
     }
 
     return (
-        <div className="w-3/4 h-1/2 ml-auto mr-auto mt-6">
-            <div className="flex h-16 justify-center items-center text-3xl">
+        <div className="w-3/4 h-1/2 ml-auto mr-auto mt-6 rounded-md">
+            <div className="flex h-16 justify-center items-center text-2xl">
                 {validate ? <h1 className="text-red-600 inline-block">Please fill all the fields before Saving user details</h1> :
                     <h1>Create New User</h1>}
             </div>
-            <div className="w-full h-1/2 my-4 flex flex-col justify-center">
+            <div className="w-full h-1/2 my-12 flex flex-col justify-center ml-6">
                 <div className="space-y-8">
                     <input
                         type="text"
                         name="first_name"
-                        className="p-5 w-1/4 rounded-md border border-gray-500 text-black mr-4"
+                        className="p-5 w-1/4 rounded-md border border-gray-600 text-black mr-4"
                         placeholder="First_name"
                         onChange={handleChange}
                     />
                     <input
                         type="text"
                         name="last_name"
-                        className="p-5 w-1/4 rounded-md border border-gray-500"
+                        className="p-5 w-1/4 rounded-md border border-gray-600"
                         placeholder="Last_name"
                         onChange={handleChange}
                     />
                     <input
                         type="text"
                         name="phone_nb"
-                        className="p-5 w-1/3 rounded-md border border-gray-500 ml-4"
+                        className="p-5 w-1/3 rounded-md border border-gray-600 ml-4"
                         placeholder="Phone Number"
                         onChange={handleChange}
                     />
                     <input
                         type="number"
                         name="age"
-                        className="p-5 w-1/5 rounded-md border border-gray-500"
+                        className="p-5 w-1/5 rounded-md border border-gray-600"
                         placeholder="Age"
                         onChange={handleChange}
                     />
                     <input
                         type="date"
                         name="date_of_birth"
-                        className="p-5 w-1/3 rounded-md border border-gray-500 ml-4"
+                        className="p-5 w-1/3 rounded-md border border-gray-600 ml-4"
                         onChange={handleChange}
                     />
                 </div>
@@ -89,38 +89,38 @@ function CreateUserModal({handleCancel} : any) {
                     <input
                         type="text"
                         name="gender"
-                        className="p-5 w-1/5 rounded-md border border-gray-500 mr-4"
+                        className="p-5 w-1/5 rounded-md border border-gray-600 mr-4"
                         placeholder="Gender"
                         onChange={handleChange}
                     />
                     <input
                         type="text"
                         name="religion"
-                        className="p-5 w-1/5 rounded-md border border-gray-500 mr-4"
+                        className="p-5 w-1/4 rounded-md border border-gray-600 mr-4"
                         placeholder="Religion (none)"
                         onChange={handleChange}
                     />
                     <input
                         type="text"
                         name="city"
-                        className="p-5 w-1/5 rounded-md border border-gray-500"
+                        className="p-5 w-1/4 rounded-md border border-gray-600"
                         placeholder="City"
                         onChange={handleChange}
                     />
                     <input
                         type="text"
                         name="disability"
-                        className="p-5 w-1/3 rounded-md border border-gray-500 ml-4"
+                        className="p-5 w-1/3 rounded-md border border-gray-600"
                         placeholder="Disability"
                         onChange={handleChange}
                     />
                 </div>
             </div>
-            <div className="w-full mt-20 text-lg text-white">
-                <button onClick={handleSave} className="p-3 float-right px-10 bg-blue-600 rounded-md ml-4">
+            <div className="w-full mt-24 text-lg text-white flex justify-center space-x-6">
+                <button onClick={handleSave} className="p-2 px-10 bg-blue-600 rounded-md ml-4">
                     Save
                 </button>
-                <button onClick={handleCancel} className="p-3 float-right px-8 bg-red-600 rounded-md">
+                <button onClick={handleCancel} className="p-2 px-8 bg-red-600 rounded-md">
                     Cancel
                 </button>
             </div>
